955c76ac2bf9edc8ddd1a9d738d593a4f31c5a4e
parker594 <debajyotiupadhyaya@gmail.com>
2025-07-09 22:58:18 +0530

    Initial commit - Major project setup

--- utils/wrapasync.js
+++ utils/wrapasync.js
@@ -1,5 +1,8 @@
  module.exports=(fn)=>{
     return function(req, res, next){
-        fn(req, res, next).catch( err =>next(err));
+        fn(req, res, next);
+          if (result && typeof result.catch === 'function') {
+      result.catch(err => next(err));
+    }
     }
 }
